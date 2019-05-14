3cd053075bce5cdac0b43cafdb661b94c58d650d
Jose Paulino <jpaulino04@msn.com>
2019-05-14 13:49:24 -0400

    added db, schema and simple routes

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express  = require('express');
 const app      = express();
+const dbConn   = require('./config/db');
 
+//Connect to mongodb
+dbConn();
 
+//Init bodyParser Middleware
+app.use(express.json({ extended: false }));
+
+//Set port
 const port = process.env.PORT || 5010;
 
-
+//Simple test route
 app.get('/', (req, res) => {
     res.send("Hello from Server")
 })
 
 
+//use your routes!
+app.use('/api/users', require('./routes/api/users'));
 
 app.listen(port, () =>{
     console.log("Server running on "+port)
