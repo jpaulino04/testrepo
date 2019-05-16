5602c1385132f097af678cc6d4794b398c5ffda8
Jose Paulino <jpaulino04@msn.com>
2019-05-16 13:57:56 -0400

    added db Schema and connected to mongo

---
+++ server.js
@@ -0,0 +1,20 @@
+const express   = require('express');
+const app       = express();
+const mongoConnect = require('./config/db')
+
+//Connect to mongo
+mongoConnect();
+
+//bodyParser
+app.use(express.json({extended: false})); // for parsing application/json
+
+const port = process.env.PORT || 5010;
+
+app.get('/', (req, res) => {
+    res.send("Keep reaching out!")
+})
+
+
+app.listen(port, ()=> {
+    console.log("Server Running on port "+ port)
+})
