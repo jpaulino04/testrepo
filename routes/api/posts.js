b81f48208c05d8fe5eba895a60748b66989473aa
Jose Paulino <jpaulino04@msn.com>
2019-06-10 15:45:00 -0400

    Started the post likes route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,45 @@ const Profile = require('../../models/Profile');
 const Post = require('../../models/Post');
 const auth  = require('../../middlewares/auth');
 
+// GET /api/posts/
+// @desc Get all posts
+// Private
+router.get('/', auth, async(req, res) => {
+
+    try {
+        const posts = await Post.find().sort({date:-1});
+
+        res.send(posts)
+    } catch (err) {
+        console.log(err.message)
+        return res.status(500).send('Server Error')
+    }
+    
+
+})
+
+//-----------------------------------------------------
+// GET /api/posts/:post_id
+// @desc Get post by post id
+// Private
+router.get('/:post_id', auth, async(req, res) => {
+
+    try {
+        const post = await Post.findById(req.params.post_id)
+
+        if(!post){
+            return res.status(404).send("Post not found")
+        }
+
+        return res.json(post)
+    } catch (err) {
+        console.error(err.message)
+        return res.status(500).send("Server Error")
+    }
+    
+})
+
+//-----------------------------------------------------
 // POST /api/posts/
 // @desc Create a new post
 // Private
@@ -31,6 +70,43 @@ async(req, res) => {
     }
 })
 
+//-----------------------------------------------------------
+// Delete /api/posts/:post_id
+// @desc Delete a post
+// Private
+router.delete('/:post_id', auth, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+        if(!post){
+            return res.status(404).send("Post not found")
+        }
+        //Note that post.user is the user id embedded in the post
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({msg: "User not authorized"})
+        }
+
+        await post.remove();
+
+        console.log("Post removed")
+
+        return res.json("Post has been removed")
+    } catch (err) {
+       return res.status(500).send('Server Error') 
+    }
+})
+
+//-----------------------------------------------------------
+// Put /api/posts/likes/:post_id
+// @desc Like a post
+// Private
+router.put('/like/:post_id', auth, async(req, res) => { 
+
+    const post = await Post.findById(req.params.post_id).sort({date: -1})
+
+    console.log(post.likes)
+    
+
+})
 
 module.exports = router;
 
