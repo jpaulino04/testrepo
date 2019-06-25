3bb9adc5030f65c10b48786c278031efe542748f
Jose Paulino <jpaulino04@msn.com>
2019-06-25 09:08:28 -0400

    Added login state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,24 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import {Link} from 'react-router-dom';
 
 const Login = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    });
+    
+    //Continue with the form and the state
+    const onChange = e => setFormData({...formData, [e.target.name]:e.target.value})
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(formData)
+    }
+
+
     return (
         <Fragment>
             <section className="container">
@@ -10,23 +27,30 @@ const Login = () => {
                 </div>
                 <h1 className="large text-primary">Sign In</h1>
                 <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
-                <form className="form" action="dashboard.html">
+                <form className="form" onSubmit = {e => onSubmit(e)}>
                     <div className="form-group">
-                    <input
-                        type="email"
-                        placeholder="Email Address"
-                        name="email"
-                        required
-                    />
+                        <input
+                            type="email"
+                            placeholder="Email Address"
+                            name="email"
+                            required
+                            onChange = {e => onChange(e)}
+                            
+                        />
                     </div>
                     <div className="form-group">
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                    />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            onChange = {e => onChange(e)}
+                        />
                     </div>
-                    <input type="submit" className="btn btn-primary" value="Login" />
+                    <input 
+                        type="submit" 
+                        className="btn btn-primary" 
+                        value="Login" 
+                    />
                 </form>
                 <p className="my-1">
                     Don't have an account? <Link to="/register">Sign Up</Link>
