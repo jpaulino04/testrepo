4e5a35f596af9e91e909b7a63796cf9fb5776f7e
Jose Paulino <jpaulino04@msn.com>
2019-06-25 12:36:59 -0400

    added state to  register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,25 +1,60 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {Link} from 'react-router-dom';
 
 const Register = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    });
+
+    const {name, email, password, password2} = formData;
+
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(formData)
+    }
     return (
         <Fragment>
             <section className="container">
                 <h1 className="large text-primary">Sign Up</h1>
                 <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-                <form className="form" action="create-profile.html">
+                <form className="form" onSubmit = { e => onSubmit(e)}>
                     <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" required />
+                        {/* use 'value' to associate this input with the name on the state */}
+                        <input 
+                            type="text" 
+                            placeholder="Name" 
+                            name="name" 
+                            onChange = { e => onChange(e)}
+                            value = {name}
+                            required 
+                        />
                     </div>
                     <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" />                    
+                        <input 
+                            type="email" 
+                            placeholder="Email Address" 
+                            name="email" 
+                            value = {email}
+                            onChange = { e => onChange(e)}
+                            required
+                        />                    
                     </div>
                     <div className="form-group">
                     <input
                         type="password"
                         placeholder="Password"
                         name="password"
+                        value = {password}
                         minLength="6"
+                        onChange = { e => onChange(e)}
+                        required
                     />
                     </div>
                     <div className="form-group">
@@ -27,7 +62,10 @@ const Register = () => {
                         type="password"
                         placeholder="Confirm Password"
                         name="password2"
+                        value = {password2}
                         minLength="6"
+                        onChange = { e => onChange(e)}
+                        required
                     />
                     </div>
                     <input type="submit" className="btn btn-primary" value="Register" />
