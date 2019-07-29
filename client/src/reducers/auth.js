6f9355b2b6b2b99b59f39d885a82ad494ea5bdf9
Jose Paulino <jpaulino04@msn.com>
2019-07-29 09:30:09 -0400

    working on auth.js reducer

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,19 @@
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+} from '../actions/types';
+
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null
+}
+
+export default function(state = initialState, action) {
+    const { type, payload } = action;
+
+    switch(type){
+        
+    }
+}
