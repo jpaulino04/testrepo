e4ff16e92718231eeacd0ffdc6f350ab5f26e0a2
Jose Paulino <jpaulino04@msn.com>
2019-07-29 09:34:28 -0400

    created auth.js reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -14,6 +14,23 @@ export default function(state = initialState, action) {
     const { type, payload } = action;
 
     switch(type){
-        
+        case REGISTER_SUCCESS:
+            localStorage.setItem('token', pauload.token);
+            return {
+                ...state, 
+                ...payload,
+                isAuthenticated: true,
+                loading: false
+            }
+        case REGISTER_FAIL:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            }
+        default:
+            return state;
     }
 }
